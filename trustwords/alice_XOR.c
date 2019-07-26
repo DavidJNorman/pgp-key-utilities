97ef665f089a60ca97b260a8f0311b8e1eb45d8c
David Norman <davidjohnnorman@outlook.com>
2019-07-26 18:17:06 +0100

    Continued to work on XOR program

--- trustwords/alice_XOR.c
+++ trustwords/alice_XOR.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc == 4) {
-        FILE* inputFile = fopen(argv[2], 'r');
-        FILE* outputFile = fopen(argv[3], "w+");
-        char line[64];
-        char aliceKey[64] = argv[1];
+        FILE *inputFile = fopen(argv[2], "r");
+        FILE *outputFile = fopen(argv[3], "w+");
+        char line[64], aliceKey[64];
         int same = 1;
+        sprintf(aliceKey, "%s", argv[1]);
         while (fgets(line, sizeof(line), inputFile)) {
             for (int i = 0; i < 40; i++) {
                 if (line[i] != aliceKey[i]) {
@@ -16,22 +16,19 @@ int main(int argc, char* argv[]) {
                 }
             }
             if (!same) {
-                int i = 0;
                 for (int b = 0; b < 5; b++) {
-                    char aliceByteStr[8];
-                    char bobByteStr[8];
-                    int aliceByte;
-                    int bobByte;
-                    int aliceXBob;
+                    char aliceByteStr[8], bobByteStr[8];
+                    long int aliceXBob;
                     for (int count = 0; count < 8; count++) {
-                        aliceByteStr[count] = aliceKey[i];
-                        bobByteStr[count] = line[i]
-                        i++;
+                        aliceByteStr[count] = aliceKey[(b * 8) + count];
+                        bobByteStr[count] = line[(b * 8) + count];
                     }
+                    printf("%08lX\n", strtol(aliceByteStr, NULL, 16));
+                    printf("%08lX\n", strtol(bobByteStr, NULL, 16));
                     aliceXBob = strtol(aliceByteStr, NULL, 16) ^ strtol(bobByteStr, NULL, 16);
-                    fprintf(outputFile, "%00000000X", aliceXBob);
+                    fprintf(outputFile, "%08lX", aliceXBob);
                 }
-                fprintf(output_file, '\n');
+                fprintf(outputFile, "\n");
             }
         }
         fclose(inputFile);
